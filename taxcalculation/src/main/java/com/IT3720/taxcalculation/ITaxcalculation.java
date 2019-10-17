f28ac58846a43a8e3a1f43f070f1a8db0e263299
Arun <Arun@LAPTOP-787UTTFT.fritz.box>
2019-10-18 01:06:32 +1300

    commit

--- taxcalculation/src/main/java/com/IT3720/taxcalculation/ITaxcalculation.java
+++ taxcalculation/src/main/java/com/IT3720/taxcalculation/ITaxcalculation.java
@@ -1,6 +1,6 @@
 package com.IT3720.taxcalculation;
 
-public interface ITaxcalculation {
+public interface ITaxcalculation  {
 	public double computeTax(double income);
 	public double weeklySalary(Employee income); 
 	public double fortnightSalary(Employee income);
--- taxcalculation/src/main/java/com/IT3720/taxcalculation/Taxcalculation.java
+++ taxcalculation/src/main/java/com/IT3720/taxcalculation/Taxcalculation.java
@@ -2,6 +2,71 @@ package com.IT3720.taxcalculation;
 
 public class Taxcalculation implements ITaxcalculation {
 
+	Employee emp;
+
+	 public  double  computeKiwiSaver(Employee income )
+	 {
+	   double kiwiSaver=0;
+	   double yearlyincome=this.getyearlyIncome(income.getIncome());
+			   
+		   kiwiSaver= yearlyincome*0.03;
+		   
+			   return kiwiSaver;
+	 }
+	   
+	
+	    public double getYearlyIncome(double income)
+	    {
+	    	double yearlyincome=0;
+	    	if(income>=0 && income<=250)
+	    	{
+	    		yearlyincome=income*40*52;
+	        } 
+	    	else if(income>=251&&income<=5000)
+	    	{
+	    		yearlyincome= income*52;
+	    	}
+	    	else if(income>=5001)
+	    	{
+	    		yearlyincome=income;
+	    	}
+		 return yearlyincome;
+	}
+	    
+	    
+	    public double computeTax(double income )
+	    {
+	    	double tax=0;
+	    	double yearlyincome=this.getYearlyIncome(income);
+	    	if(yearlyincome>=0 && yearlyincome<=14000)
+	    	{
+	    		tax=yearlyincome*0.105;
+	    	}
+	    	else if(yearlyincome>=14000&&yearlyincome<=48000)
+	    	{
+	    		
+	    		tax=(yearlyincome-14001)*17.5+14000*0105;
+	    		
+	    	}
+	    	else if(yearlyincome >=70000)
+	    	{
+	    		tax=(yearlyincome-7001)*0.33 + (48000-14000)*0.175 + (70000-48001)*0.30 - 14000*0.105;
+	    		
+	    	}
+	    	
+	    	return tax;
+	    }
+	    	
+	    	
+	    		
+	    	
+	    
+	 
+	
+	
+	
+	
+		
 	
 	public double yearlyIncome(double income)
 	{
@@ -13,11 +78,7 @@ public class Taxcalculation implements ITaxcalculation {
 	 * @param income
 	 * @return
 	 */
-	public double computeTax(double income) {
-		double  tax=0;
-		double yearlyIncome=this.getyearlyIncome(income);
-		return 0;
-	}
+	
 
 	
 	public double weeklySalary(Employee income) {
@@ -30,13 +91,17 @@ public class Taxcalculation implements ITaxcalculation {
 		return 0;
 	}
 
-	public double computeKiwiSaver(Employee income) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	
+	
 	public double getyearlyIncome(double income) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+
+	public void emp(ITaxcalculation mockobj) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
